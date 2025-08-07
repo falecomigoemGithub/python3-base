b71f191cf0bd59e6cb75305fb1c35603a71471e7
Angelo Almeida <falecomigoem@gmail.com>
2025-08-07 23:10:47 +0000

    Atualização DUNDER

--- hello.py
+++ hello.py
@@ -1,3 +1,21 @@
 #! /usr/bin/python3
+"""Hello World Multi Línguas.
 
+Dependendo da língua configurada no ambiente o programa exibe a mensagem
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+Execução:
+
+    python4 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Bruno Rocha"
+__license__ = "Unlicense"
 print("Hello, World!")
